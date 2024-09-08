fdb7278e670bdce6c4c1b5e9c93917d1a1f555c1
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-08 03:07:36 +0000

    add title and description

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,10 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
-export default defineConfig({});
+export default defineConfig({
+  manifest: {
+    default_locale: "en",
+    name: "__MSG_name__",
+    description: "__MSG_desc__",
+  },
+});
